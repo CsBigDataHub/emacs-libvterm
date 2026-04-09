1605676bac4d60e31aca9c792075e46ef3c746ee
CsBigDataHub <kchetan.hadoop@gmail.com>
2026-04-09 12:38:48 -0400

    remove unneeded files

--- vterm-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from vterm.el  -*- no-byte-compile: t -*-
-(define-package "vterm" "0.0.2" "No description available." '((emacs "25.1")) :kind vc :commit "8d564fd81840d1e8194541a10726ab394620721b")
